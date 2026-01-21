a411fdad3795bf9f155af7fa4f7516d651d74e00
RunnelleMoredo <runnellematthew.moredo@ontariotechu.net>
2026-01-21 13:19:05 -0500

    started backend code, worked on tabs

--- app/(tabs)/workout.jsx
+++ app/(tabs)/workout.jsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Pressable } from 'react-native'
+import { router } from 'expo-router'
 import React from 'react'
 
 const workout = () => {
     return (
-        <View>
-            <Text>workout</Text>
+        <View className="flex-1 justify-center items-center">
+            <Pressable onPress={() => router.push("/workout_session")}>
+                <Text className="text-5xl text-dark-200 font-bold">Start Workout</Text>
+            </Pressable>
         </View>
+        
     )
 }   
 
 export default workout
 
 const styles = StyleSheet.create({})
+
---
+++ app/workout_session.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class workout_session extends Component {   
+    render() {
+        return (
+            <View>
+                <Text> here fuck </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({})
