215c140dbc54b860bafd90c72a03d2fc0683e3ef
RunnelleMoredo <runnellematthew.moredo@ontariotechu.net>
2026-01-25 23:09:08 -0500

    added search exercise and set and verify password in models

--- backend/routes/exercises.py
+++ backend/routes/exercises.py
@@ -3,17 +3,53 @@ import os
 import requests
 
 EXERCISE_API_URL = os.getenv("EXERCISE_API_URL") 
-# EXERCISE_API_URL = "https://www.exercisedb.dev/api/v1/exercises"
 
 if not EXERCISE_API_URL:
     raise RuntimeError("EXERCISE_API_URL not properly set")
 
 exercise_bp = Blueprint("exercise", __name__)
 
-
-@exercise_bp.route("/all", methods=["GET"])
-def get_all_workouts():
+#get all exercises
+@exercise_bp.route("/", methods=["GET"])
+def get_all_exercises():
     response = requests.get(EXERCISE_API_URL)
     response.raise_for_status()
     
     return jsonify(response.json()), 200
+
+#search exercise
+@exercise_bp.route("/search", methods=["GET"])
+def search_exercise():
+    
+    query = request.args.get('q', '')
+    if not query:
+        return jsonify({"error": "Missing search query"}), 400
+    
+    response = requests.get(
+        EXERCISE_API_URL + "/search",
+        params={"q": query}, 
+        timeout= 5
+    )
+    
+    
+    if response.status_code != 200:
+        return jsonify({
+            "error": "Upstream API error",
+            "status": response.status_code
+        }), 502
+    
+    data = response.json()
+    
+    cleaned = []
+    
+    for exercise in data.get("data", []):
+        cleaned.append({
+            "name": exercise.get("name"),
+            "equipments": exercise.get("equipments"),
+            "instructions": exercise.get("instructions"),
+            "targetMuscles": exercise.get("targetMuscles"),
+            "secondaryMuscles": exercise.get("secondaryMuscles"),
+            "gifUrl": exercise.get("gifUrl"),
+        })
+        
+    return jsonify(cleaned), 200        
